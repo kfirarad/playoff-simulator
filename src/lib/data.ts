4a00b9ea9f44b2c2d42a5cfeb2794496e60772df
Kfir Arad <the.kfir@gmail.com>
2026-02-15 21:40:13 +0100

    Update 2026-02-15

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -35,7 +35,7 @@ export const initialData: LeagueData = {
     { id: "m23-3", homeTeamId: "HKS", awayTeamId: "ET", homeGoals: 2, awayGoals: 2, played: true, gameWeek: 23 },
     { id: "m23-4", homeTeamId: "MBR", awayTeamId: "MN", homeGoals: 0, awayGoals: 4, played: true, gameWeek: 23 },
     { id: "m23-5", homeTeamId: "MSA", awayTeamId: "HBS", homeGoals: 1, awayGoals: 2, played: false, gameWeek: 23 },
-    { id: "m23-6", homeTeamId: "MH", awayTeamId: "BS", homeGoals: null, awayGoals: null, played: false, gameWeek: 23 },
+    { id: "m23-6", homeTeamId: "MH", awayTeamId: "BS", homeGoals: 4, awayGoals: 0, played: true, gameWeek: 23 },
     { id: "m23-7", homeTeamId: "BJ", awayTeamId: "MTA", homeGoals: null, awayGoals: null, played: false, gameWeek: 23 },
 
     // Round 24
